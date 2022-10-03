90d5df3ce93378263969bba4db9ce0fe03eb02ad
VIJAYWHAT <vijayrv042@gmail.com>
2022-10-03 18:31:45 +0530

    star pattern in c

---
+++ pattent.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+
+    /*int i,j,n;
+
+    printf("enter the n:");
+    scanf("%d",&n);
+
+       for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+           
+            printf("*");
+        }
+        printf("\n");
+
+       }*/
+
+        int i,j,row,col;
+
+    printf("enter the row:");
+    scanf("%d",&row);
+
+    printf("enter the col:");
+    scanf("%d",&col);
+
+       for(i=1;i<=row;i++){
+        for(j=1;j<=col;j++){
+           
+            printf("*");
+        }
+        printf("\n");
+
+       }
+
+}
