2d1a95b1402096c7d89f351875ee1d80108ea4f7
VIJAYWHAT <vijayrv042@gmail.com>
2022-10-10 06:18:57 +0530

    string

--- find_sum_in_array.c
+++ find_sum_in_array.c
@@ -8,6 +8,7 @@ int main(){
 
     for(int i=0;i<n;i++){
         for(int j=i+1 ;j<n;j++){
+            
             if(arr[i]+arr[j] == sum){
                 printf("%d + %d = %d \n",arr[i],arr[j],sum);
             }
---
+++ string_length.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    int n = 100;
+    int i = 0;
+    char name[n];
+    int length = 0;
+
+    printf("enter the name: ");
+    fgets(name,n,stdin);
+
+    while (name[i] != '\0')
+    {
+        length++;
+        i++;
+
+    }
+            
+        printf("length of string: %d\n",length);
+}
